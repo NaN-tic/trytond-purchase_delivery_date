1b4fdae133ba7863c845ed29c4f5c0796ade07e2
Eduardo Basalo Pe?a <eduardo@nan-tic.com>
2017-03-16 14:19:40 +0100

    Chages in purchase.py file to consider the renamed field from manual_delivery_date to requested_delivery_date. Added view purchase_line_form.xml to add the requested_delivery_date field of the model purchase.line (in the file purchase.py of the module purchase_delivery_date)

--- purchase.py
+++ purchase.py
@@ -12,18 +12,13 @@ __metaclass__ = PoolMeta
 
 class PurchaseLine:
     __name__ = 'purchase.line'
-    manual_delivery_date = fields.Date('Delivery Date',
-            states={
-                'invisible': ((Eval('type') != 'line')
-                    | (If(Bool(Eval('quantity')), Eval('quantity', 0), 0)
-                        <= 0)),
-                },
-            depends=['type', 'quantity'])
-
-    @classmethod
-    def __setup__(cls):
-        super(PurchaseLine, cls).__setup__()
-        cls.delivery_date.states['invisible'] = True
+    requested_delivery_date = fields.Date('Fecha de entrega requerida',
+        states={
+            'invisible': ((Eval('type') != 'line')
+                | (If(Bool(Eval('quantity')), Eval('quantity', 0), 0)
+                    <= 0)),
+        },
+        depends=['type', 'quantity'])
 
     @classmethod
     def __register__(cls, module_name):
@@ -33,25 +28,36 @@ class PurchaseLine:
 
         # Migration from 3.2
         table = TableHandler(cls, module_name)
-        move_delivery_dates = (not table.column_exist('manual_delivery_date')
-            and table.column_exist('delivery_date'))
+        move_delivery_dates = (
+            not table.column_exist('requested_delivery_date')
+            and table.column_exist('delivery_date')
+        )
+
+        # Because of the change of the field's name manual_delivery_date to
+        # requested_delivery_date
+        if (table.column_exist('manual_delivery_date')
+                and not table.column_exist('requested_delivery_date')):
+            table.column_rename('manual_delivery_date',
+                'requested_delivery_date')
 
         super(PurchaseLine, cls).__register__(module_name)
 
         if move_delivery_dates:
             cursor.execute(*sql_table.update(
-                    columns=[sql_table.manual_delivery_date],
-                    values=[sql_table.delivery_date]))
+                columns=[sql_table.requested_delivery_date],
+                values=[sql_table.delivery_date]))
             table.drop_column('delivery_date')
 
-    @fields.depends('manual_delivery_date', methods=['delivery_date'])
-    def on_change_with_manual_delivery_date(self):
-        if self.manual_delivery_date:
-            return self.manual_delivery_date
+    @fields.depends('requested_delivery_date', methods=['delivery_date'])
+    def on_change_with_requested_delivery_date(self):
+        if self.requested_delivery_date:
+            return self.requested_delivery_date
         return super(PurchaseLine,
             self).on_change_with_delivery_date(name='delivery_date')
 
-    @fields.depends('manual_delivery_date')
+    @fields.depends('requested_delivery_date', 'moves')
     def on_change_with_delivery_date(self, name=None):
-        return self.manual_delivery_date
-
+        if self.moves:
+            return super(PurchaseLine, self).on_change_with_delivery_date()
+        else:
+            return self.requested_delivery_date
